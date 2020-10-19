8747f4ec28d984291656091b34b7c3e8bbd32d13
Helbert M Pinto <helbert.tec.info@gmail.com>
2020-10-19 19:14:17 -0300

    adjusting attribs positions

--- server/src/Champion.cpp
+++ server/src/Champion.cpp
@@ -13,22 +13,46 @@ Champion::Champion(string name) {
     this->name = name;
 
     if (name == "Garen") {
-        this->attribs = new Attribute(GAREN[0], GAREN[1], GAREN[2], GAREN[3], GAREN[4], GAREN[5], GAREN[6]);
+        this->attribs = new Attribute(
+            GAREN[0], GAREN[1],
+            GAREN[2], GAREN[3],
+            GAREN[4], GAREN[5],
+            GAREN[6]);
         this->type = "TANK";
     } else if (name == "Lux") {
-        this->attribs = new Attribute(LUX[0], LUX[1], LUX[2], LUX[3], LUX[4], LUX[5], LUX[6]);
+        this->attribs = new Attribute(
+            LUX[0], LUX[1],
+            LUX[2], LUX[3],
+            LUX[4], LUX[5],
+            LUX[6]);
         this->type = "MAGE";
     } else if (name == "Ashe") {
-        this->attribs = new Attribute(ASHE[0], ASHE[1], ASHE[2], ASHE[3], ASHE[4], ASHE[5], ASHE[6]);
+        this->attribs = new Attribute(
+            ASHE[0], ASHE[1],
+            ASHE[2], ASHE[3],
+            ASHE[4], ASHE[5],
+            ASHE[6]);
         this->type = "RANGED";
     } else if (name == "Darius") {
-        this->attribs = new Attribute(DARIUS[0], DARIUS[1], DARIUS[2], DARIUS[3], DARIUS[4], DARIUS[5], DARIUS[6]);
+        this->attribs = new Attribute(
+            DARIUS[0], DARIUS[1],
+            DARIUS[2], DARIUS[3],
+            DARIUS[4], DARIUS[5],
+            DARIUS[6]);
         this->type = "TANK";
     } else if (name == "Ryze") {
-        this->attribs = new Attribute(RYZE[0], RYZE[1], RYZE[2], RYZE[3], RYZE[4], RYZE[5], RYZE[6]);
+        this->attribs = new Attribute(
+            RYZE[0], RYZE[1],
+            RYZE[2], RYZE[3],
+            RYZE[4], RYZE[5],
+            RYZE[6]);
         this->type = "MAGE";
     } else if (name == "Vayne") {
-        this->attribs = new Attribute(VAYNE[0], VAYNE[1], VAYNE[2], VAYNE[3], VAYNE[4], VAYNE[5], VAYNE[6]);
+        this->attribs = new Attribute(
+            VAYNE[0], VAYNE[1],
+            VAYNE[2], VAYNE[3],
+            VAYNE[4], VAYNE[5],
+            VAYNE[6]);
         this->type = "RANGED";
     }
 }
--- server/src/Item.cpp
+++ server/src/Item.cpp
@@ -23,142 +23,100 @@ Item::Item(string name) {
     if (name == "Infinity Edge") {
         this->id = 1;
         this->attribs = new Attribute(
-            INFINITY_EDGE[0],
-            INFINITY_EDGE[1],
-            INFINITY_EDGE[2],
-            INFINITY_EDGE[3],
-            INFINITY_EDGE[4],
-            INFINITY_EDGE[5],
+            INFINITY_EDGE[0], INFINITY_EDGE[1],
+            INFINITY_EDGE[2], INFINITY_EDGE[3],
+            INFINITY_EDGE[4], INFINITY_EDGE[5],
             INFINITY_EDGE[6]);
     } else if (name == "Rabbadons") {
         this->id = 2;
         this->attribs = new Attribute(
-            RABBADONS_DEATH_CAP[0],
-            RABBADONS_DEATH_CAP[1],
-            RABBADONS_DEATH_CAP[2],
-            RABBADONS_DEATH_CAP[3],
-            RABBADONS_DEATH_CAP[4],
-            RABBADONS_DEATH_CAP[5],
+            RABBADONS_DEATH_CAP[0], RABBADONS_DEATH_CAP[1],
+            RABBADONS_DEATH_CAP[2], RABBADONS_DEATH_CAP[3],
+            RABBADONS_DEATH_CAP[4], RABBADONS_DEATH_CAP[5],
             RABBADONS_DEATH_CAP[6]);
     } else if (name == "Thornmail") {
         this->id = 3;
         this->attribs = new Attribute(
-            THORNMAIL[0],
-            THORNMAIL[1],
-            THORNMAIL[2],
-            THORNMAIL[3],
-            THORNMAIL[4],
-            THORNMAIL[5],
+            THORNMAIL[0], THORNMAIL[1],
+            THORNMAIL[2], THORNMAIL[3],
+            THORNMAIL[4], THORNMAIL[5],
             THORNMAIL[6]);
     } else if (name == "Spirity Visage") {
         this->id = 4;
         this->attribs = new Attribute(
-            SPIRITY_VISAGE[0],
-            SPIRITY_VISAGE[1],
-            SPIRITY_VISAGE[2],
-            SPIRITY_VISAGE[3],
-            SPIRITY_VISAGE[4],
-            SPIRITY_VISAGE[5],
+            SPIRITY_VISAGE[0], SPIRITY_VISAGE[1],
+            SPIRITY_VISAGE[2], SPIRITY_VISAGE[3],
+            SPIRITY_VISAGE[4], SPIRITY_VISAGE[5],
             SPIRITY_VISAGE[6]);
     } else if (name == "Warmogs") {
         this->id = 5;
         this->attribs = new Attribute(
-            WARMOGS_ARMOR[0],
-            WARMOGS_ARMOR[1],
-            WARMOGS_ARMOR[2],
-            WARMOGS_ARMOR[3],
-            WARMOGS_ARMOR[4],
-            WARMOGS_ARMOR[5],
+            WARMOGS_ARMOR[0], WARMOGS_ARMOR[1],
+            WARMOGS_ARMOR[2], WARMOGS_ARMOR[3],
+            WARMOGS_ARMOR[4], WARMOGS_ARMOR[5],
             WARMOGS_ARMOR[6]);
     } else if (name == "Seraph Embrace") {
         this->id = 6;
         this->attribs = new Attribute(
-            SERAPH_EMBRACE[0],
-            SERAPH_EMBRACE[1],
-            SERAPH_EMBRACE[2],
-            SERAPH_EMBRACE[3],
-            SERAPH_EMBRACE[4],
-            SERAPH_EMBRACE[5],
+            SERAPH_EMBRACE[0], SERAPH_EMBRACE[1],
+            SERAPH_EMBRACE[2], SERAPH_EMBRACE[3],
+            SERAPH_EMBRACE[4], SERAPH_EMBRACE[5],
             SERAPH_EMBRACE[6]);
     } else if (name == "Boots") {
         this->id = 7;
         this->attribs = new Attribute(
-            BOOTS_SWIFTNESS[0],
-            BOOTS_SWIFTNESS[1],
-            BOOTS_SWIFTNESS[2],
-            BOOTS_SWIFTNESS[3],
-            BOOTS_SWIFTNESS[4],
-            BOOTS_SWIFTNESS[5],
+            BOOTS_SWIFTNESS[0], BOOTS_SWIFTNESS[1],
+            BOOTS_SWIFTNESS[2], BOOTS_SWIFTNESS[3],
+            BOOTS_SWIFTNESS[4], BOOTS_SWIFTNESS[5],
             BOOTS_SWIFTNESS[6]);
     } else if (name == "Adaptive Helm") {
         this->id = 8;
         this->attribs = new Attribute(
-            REDEMPTION[0],
-            REDEMPTION[1],
-            REDEMPTION[2],
-            REDEMPTION[3],
-            REDEMPTION[4],
-            REDEMPTION[5],
+            REDEMPTION[0], REDEMPTION[1],
+            REDEMPTION[2], REDEMPTION[3],
+            REDEMPTION[4], REDEMPTION[5],
             REDEMPTION[6]);
     } else if (name == "Blade Ruined King") {
         this->id = 9;
         this->attribs = new Attribute(
-            BLADE_RUINED_KING[0],
-            BLADE_RUINED_KING[1],
-            BLADE_RUINED_KING[2],
-            BLADE_RUINED_KING[3],
-            BLADE_RUINED_KING[4],
-            BLADE_RUINED_KING[5],
+            BLADE_RUINED_KING[0], BLADE_RUINED_KING[1],
+            BLADE_RUINED_KING[2], BLADE_RUINED_KING[3],
+            BLADE_RUINED_KING[4], BLADE_RUINED_KING[5],
             BLADE_RUINED_KING[6]);
     } else if (name == "Black Cleaver") {
         this->id = 10;
         this->attribs = new Attribute(
-            BLACK_CLEAVER[0],
-            BLACK_CLEAVER[1],
-            BLACK_CLEAVER[2],
-            BLACK_CLEAVER[3],
-            BLACK_CLEAVER[4],
-            BLACK_CLEAVER[5],
+            BLACK_CLEAVER[0], BLACK_CLEAVER[1],
+            BLACK_CLEAVER[2], BLACK_CLEAVER[3],
+            BLACK_CLEAVER[4], BLACK_CLEAVER[5],
             BLACK_CLEAVER[6]);
     } else if (name == "Liandry") {
         this->id = 11;
         this->attribs = new Attribute(
-            LIANDRY_TORNENT[0],
-            LIANDRY_TORNENT[1],
-            LIANDRY_TORNENT[2],
-            LIANDRY_TORNENT[3],
-            LIANDRY_TORNENT[4],
-            LIANDRY_TORNENT[5],
+            LIANDRY_TORNENT[0], LIANDRY_TORNENT[1],
+            LIANDRY_TORNENT[2], LIANDRY_TORNENT[3],
+            LIANDRY_TORNENT[4], LIANDRY_TORNENT[5],
             LIANDRY_TORNENT[6]);
     } else if (name == "Banshee") {
         this->id = 12;
         this->attribs = new Attribute(
-            BANSHEE_VEIL[0],
-            BANSHEE_VEIL[1],
-            BANSHEE_VEIL[2],
-            BANSHEE_VEIL[3],
-            BANSHEE_VEIL[4],
-            BANSHEE_VEIL[5],
+            BANSHEE_VEIL[0], BANSHEE_VEIL[1],
+            BANSHEE_VEIL[2], BANSHEE_VEIL[3],
+            BANSHEE_VEIL[4], BANSHEE_VEIL[5],
             BANSHEE_VEIL[6]);
     } else if (name == "Runaans") {
         this->id = 13;
         this->attribs = new Attribute(
-            RUNAANS_HURRICANE[0],
-            RUNAANS_HURRICANE[1],
-            RUNAANS_HURRICANE[2],
-            RUNAANS_HURRICANE[3],
-            RUNAANS_HURRICANE[4],
-            RUNAANS_HURRICANE[5],
+            RUNAANS_HURRICANE[0], RUNAANS_HURRICANE[1],
+            RUNAANS_HURRICANE[2], RUNAANS_HURRICANE[3],
+            RUNAANS_HURRICANE[4], RUNAANS_HURRICANE[5],
             RUNAANS_HURRICANE[6]);
     } else if (name == "Trynity Force") {
         this->id = 14;
         this->attribs = new Attribute(
-            TRINITY_FORCE[0],
-            TRINITY_FORCE[1],
-            TRINITY_FORCE[2],
-            TRINITY_FORCE[3],
-            TRINITY_FORCE[4],
-            TRINITY_FORCE[5],
+            TRINITY_FORCE[0], TRINITY_FORCE[1],
+            TRINITY_FORCE[2], TRINITY_FORCE[3],
+            TRINITY_FORCE[4], TRINITY_FORCE[5],
             TRINITY_FORCE[6]);
     }
 }
