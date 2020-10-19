c9d4d202868115a9a8fd9ecc7fa45ecb194aa3d9
George Gantus <georgegantus@usp.br>
2020-10-18 22:56:29 -0300

    item and spell get and set

--- client/src/Item.cpp
+++ client/src/Item.cpp
@@ -1,7 +1,19 @@
 #include <Attributes.hpp>
 #include <Item.hpp>
 
-Item::Item(string description, Attributes *attribs) {
+Item::Item(int id, string name, string description, Attributes* attribs) {
     this->attribs = attribs;
     this->description = description;
+}
+int Item::getId() {
+    return this->id;
+}
+string Item::getName() {
+    return this->name;
+}
+string Item::getDescription() {
+    return this->description;
+}
+Attributes* Item::getAttributes() {
+    return this->attribs;
 }
--- client/src/Spell.cpp
+++ client/src/Spell.cpp
@@ -6,3 +6,15 @@ Spell::Spell(string name, string effect, int value, int manaCost) {
     this->value = value;
     this->manaCost = manaCost;
 }
+string Spell::getName() {
+    return this->name;
+}
+string Spell::getEffect() {
+    return this->effect;
+}
+int Spell::getManaCost() {
+    return this->manaCost;
+}
+int Spell::getValue() {
+    return this->value;
+}
