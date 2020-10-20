da9f0fb99ac7421e1715e862eb4538c26ace1b41
Helbert M Pinto <helbert.tec.info@gmail.com>
2020-10-20 18:46:51 -0300

    add lalala

--- client/src/Player.cpp
+++ client/src/Player.cpp
@@ -1,1 +1,19 @@
 #include "../headers/Player.hpp"
+
+Player::Player(int id, string nickname, int championId) {
+    this->id = id;
+    this->nickname = nickname;
+    this->champion = new Champion(championId);
+}
+
+int Player::getId() {
+    return this->id;
+}
+
+string Player::getNickname() {
+    return this->nickname;
+}
+
+Champion* Player::getChampion() {
+    return this->champion;
+}
