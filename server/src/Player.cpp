9014bce22fdd6fc5cd586ea5cd548b1ff23fc4eb
George Gantus <georgegantus@usp.br>
2020-10-21 09:23:16 -0300

    manaflow

--- server/src/Player.cpp
+++ server/src/Player.cpp
@@ -46,6 +46,7 @@ struct moviment Player::setDamage(int action, int turn) {
         move.value = value;
         move.areaEffect = areaEffect;
         move.effects = effects;
+        this->champion->getAttribs()->setMana(this->champion->getAttribs()->getMana() - mana);
         //agora os valores de value, effects e area Effect ja estão basta causa esse dano ao target ou aos targets
         //termina o ataque
     }
