473d75cf39026702d2fcdfc50f92a79754f93da7
George Gantus <georgegantus@usp.br>
2020-10-20 21:01:03 -0300

    damage and Heal

--- server/main.cpp
+++ server/main.cpp
@@ -19,7 +19,7 @@ int main(int argc, char* argv[]) {
     struct instruction action;
     struct moviment move;
     for (int i = 0; i < MAX_CONNECTIONS; i++) {
-        players.push_back(server->getPlayers()[i])
+        players.push_back(server->getPlayers()[i]);
     }
     //neste ponto a lista de players esta ok na teoria
     sort(players.begin(), players.end(), compareSpeed);
@@ -28,12 +28,28 @@ int main(int argc, char* argv[]) {
         //representa a partida
         turn++;
         for (int i = 0; i < MAX_CONNECTIONS; i++) {  //representa um turno
-            currentPlayer = &players[i];
+            currentPlayer = players[i];
             action = server->waitForInstruction(currentPlayer);
             move = currentPlayer->setDamage(action.type, turn);
             if (move.areaEffect == true) {
+                for (int i = 0; i < MAX_CONNECTIONS; i++) {
+                    players[i]->getChampion()->getDamage(move.value);
+                }
                 //impacta em todos os players
             } else {
+                if (move.type == damage) {
+                    for (int i = 0; i < MAX_CONNECTIONS; i++) {
+                        if (players[i]->getId() == action.target) {
+                            players[i]->getChampion()->getDamage(move.value)
+                        }
+                    }
+                } else {
+                    for (int i = 0; i < MAX_CONNECTIONS; i++) {
+                        if (players[i]->getId() == action.target) {
+                            players[i]->getChampion()->getHealed(move.value)
+                        }
+                    }
+                }
                 //impacto apenas no target
             }
             //wait for instruction
