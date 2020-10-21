37cae14da252e177282b1f8254e7b4ed1e21dcc0
Helbert M Pinto <helbert.tec.info@gmail.com>
2020-10-21 00:51:59 -0300

    impedindo jogador morto a jogar

--- server/src/Server.cpp
+++ server/src/Server.cpp
@@ -1,33 +1,11 @@
 #include "../headers/Server.hpp"
-/*
-void *Server::connection_listener(void *clientFd) {
-    int client = *((int *)clientFd);
-    int read_size;
-    char buffer[256];
-    std::cout << "[INFO] Connection listener created" << std::endl;
-    int recived_bytes;
-    while (1) {
-        memset(buffer, 0, 256);
-        recived_bytes = recv(client, buffer, 256, 0);
-        buffer[recived_bytes] = '\0';
-        printf("Client: %s", buffer);
-    }
-}
 
-void *Server::connection_sender(void *clientFd) {
-    int client = *(int *)clientFd;
-    std::cout << "[INFO] Connection sender created" << std::endl;
-    while (1) {
-        char buffer[256];
-        fgets(buffer, 256, stdin);
-        send(client, buffer, strlen(buffer), 0);
-    }
-}
-*/
+#include "../headers/Globals.hpp"
+
 Server::Server(int port) {
     socketFd = socket(AF_INET, SOCK_STREAM, 0);
     if (socketFd == -1) {
-        std::cout << "[ERROR] Failed to create socket!" << std::endl;
+        cout << "[ERROR] Failed to create socket!" << endl;
         return;
     }
     server.sin_family = AF_INET;
@@ -35,7 +13,7 @@ Server::Server(int port) {
     server.sin_port = htons(port);
     memset(&server.sin_zero, 0, sizeof(server.sin_zero));
     if (bind(socketFd, (struct sockaddr *)&server, sizeof(server)) < 0) {
-        std::cout << "[ERROR] Failed to bind!" << std::endl;
+        cout << "[ERROR] Failed to bind!" << endl;
         return;
     }
     return;
@@ -49,15 +27,11 @@ void Server::listenForClients() {
     int socket_client;
     while ((socket_client = accept(socketFd, (struct sockaddr *)&client, (socklen_t *)&sizeSock))) {
         if (socket_client < 0) {
-            std::cout << "[ERROR] Connection Error " << std::endl;
+            cout << "[ERROR] Connection Error " << endl;
             continue;
         }
-        std::cout << "[INFO] New connection accepted (IP " << inet_ntoa(client.sin_addr) << ":" << client.sin_port << ")" << std::endl;
+        cout << "[INFO] New connection accepted (IP " << inet_ntoa(client.sin_addr) << ":" << client.sin_port << ")" << endl;
         this->setUpPlayer(socket_client, cli_id);
-        /*
-        th_clients_sender[cli_id] = std::thread(connection_sender, (void *)&socket_client[cli_id]);
-        th_clients_listener[cli_id] = std::thread(connection_listener, (void *)&socket_client[cli_id]);
-        */
         cli_id++;
         if (cli_id == MAX_CONNECTIONS)
             break;
@@ -76,13 +50,13 @@ void Server::setUpPlayer(int socket_client, int cli_id) {
     int item[2];
     recv(socket_client, &item[0], sizeof(int), 0);
     recv(socket_client, &item[1], sizeof(int), 0);
-    std::string name(name_c);
+    string name(name_c);
     this->playerlist.push_back(new Player(cli_id, name, socket_client, champion, item[0], item[1]));
 
-    std::cout << "[INFO] Player setted up. Name: " << this->playerlist[cli_id]->getNickname();
-    std::cout << " / Champion: " << this->playerlist[cli_id]->getChampion()->getName();
-    std::cout << " / Item 0: " << this->playerlist[cli_id]->getChampion()->getItem()[0]->getName();
-    std::cout << " / Item 1: " << this->playerlist[cli_id]->getChampion()->getItem()[1]->getName() << std::endl;
+    cout << "[INFO] Player setted up. Name: " << this->playerlist[cli_id]->getNickname();
+    cout << " / Champion: " << this->playerlist[cli_id]->getChampion()->getName();
+    cout << " / Item 0: " << this->playerlist[cli_id]->getChampion()->getItem()[0]->getName();
+    cout << " / Item 1: " << this->playerlist[cli_id]->getChampion()->getItem()[1]->getName() << endl;
 
     send(socket_client, &cli_id, sizeof(int), 0);
     return;
@@ -106,7 +80,7 @@ void Server::sendPlayers() {
             champion = this->playerlist[j]->getChampion()->getId();
             item[0] = this->playerlist[j]->getChampion()->getItem()[0]->getId();
             item[1] = this->playerlist[j]->getChampion()->getItem()[1]->getId();
-            std::cout << name << " " << champion << " " << item[0] << " " << item[1];
+            cout << name << " " << champion << " " << item[0] << " " << item[1];
             send(this->playerlist[i]->getSocket(), &i, sizeof(int), 0);
             send(this->playerlist[i]->getSocket(), &name, 20, 0);
             send(this->playerlist[i]->getSocket(), &champion, sizeof(int), 0);
